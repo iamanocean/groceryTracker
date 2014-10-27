db4e1409ce27f2be708a95501f04ab5779158ce4
iamanocean <iamanocean@me.com>
2014-10-26 18:16:05 -0600

    Adds Camera View and changes test image

--- groceryTracker/ViewController.swift
+++ groceryTracker/ViewController.swift
@@ -7,24 +7,53 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var myImage: UIImage = UIImage(named: "IMG_0240.JPG")!
+    var flag: Bool = false
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+            println("I've got an image")
+        
+            picker.presentingViewController?.dismissViewControllerAnimated(true, completion: { () -> Void in
+            })
+    }
+    
+    @IBOutlet var textLabel: UILabel!
+    @IBAction func addImageFromCamera(sender: UIButton) {
+        var imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.Camera
+        imagePickerController.allowsEditing = false
+        self.presentViewController(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    func recognizeImage() {
+        println("Started to recognize text")
+        let tesseract = Tesseract(language: "eng+ita")
+        tesseract.image = myImage.grayScale()
+        tesseract.recognize()
+        println(tesseract.recognizedText)
+        textLabel.text = tesseract.recognizedText
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let tesseract = Tesseract(language: "eng+ita")
-        
-        let image = UIImage(named: "image_sample.jpg")
+    
         
         
-        tesseract.setVariableValue("0123456789", forKey: "tessedit_char_whitelist")
-        tesseract.image = image.blackAndWhite()
-        tesseract.recognize();
         
-        println(tesseract.recognizedText)
-        println("Hello, World!")
-    }
+        self.recognizeImage()
+    
+        
+        
+        // tesseract.setVariableValue("0123456789", forKey: "tessedit_char_whitelist")
+       
+        
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
